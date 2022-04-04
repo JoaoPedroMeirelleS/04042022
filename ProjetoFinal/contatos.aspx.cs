6c3dfda029d5b633d7077f8fb568c23137dbbcbc
JoaoPedroMeirelleS <94013155+JoaoPedroMeirelleS@users.noreply.github.com>
2022-04-04 17:16:23 -0300

    Pojeto Final 2.0
    Projeto mais atualizadoooooooo !!!!!!!!!!!!!!!!

---
+++ ProjetoFinal/contatos.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjetoFinal
+{
+    public partial class contatos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            //capturar a string de conexão
+            System.Configuration.Configuration rootWebConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/MyWebSiteRoot");
+            // Criado pelo SQL Data Source
+            System.Configuration.ConnectionStringSettings connString;
+            connString = rootWebConfig.ConnectionStrings.ConnectionStrings["ConnectionString"];
+            //cria um objeto de conexão
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connString.ToString();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            // Chamada da Inserção pelo SQL insert, escolha da tabela into, campos (nome,email,senha) e variáveis (@nome,@email,@senha)
+            cmd.CommandText = "Insert into contatos (nome,email,assunto,mensagem) values (@nome,@email,@assunto,@mensagem)";
+
+            // Varáveis do banco de dados iniciado por @ arroba
+            cmd.Parameters.AddWithValue("nome", TextBox1.Text);
+            cmd.Parameters.AddWithValue("email", TextBox4.Text);
+            cmd.Parameters.AddWithValue("assunto", TextBox2.Text);
+            cmd.Parameters.AddWithValue("mensagem", TextBox3.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+           
+        }
+
+    }
+}
